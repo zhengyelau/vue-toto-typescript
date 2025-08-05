7164c11d595775f1c530b37d8350cf05caa2c4c7
Zheng ye <zhengye@Mac.singtel.com>
2025-08-05 11:20:51 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue'
+import AppHeader from './components/AppHeader.vue'
+</script>
+
+<template>
+  <TodoList />
+  <AppHeader />
+</template>
+
+<style scoped></style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+interface CheckBoxCheck {
+  isChecked: boolean | false
+}
+
+defineProps<CheckBoxCheck>()
+</script>
+
+<template>
+  <label :class="{ checked: isChecked }">
+    <input type="checkbox" :checked="isChecked" />
+    <slot></slot>
+  </label>
+</template>
+
+<style scoped>
+label {
+  cursor: pointer;
+}
+
+.checked {
+  text-decoration: line-through;
+}
+</style>
